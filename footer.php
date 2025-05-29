25861abb65c2847b94b12f6874aecc4912f43987
Primael75 <primaelazinnongbe4@gmail.com>
2025-05-29 02:13:10 +0100

    ajout du footer

---
+++ footer.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+    <!--footer-->
+    <footer>
+        <img src="logo/footerimg.avif" alt="Footer Image" class="footer-image">
+
+        <div class="footer-container">
+            <div class="footer-links">
+                <div class="footer-column"> 
+                    <h4>Besoin d'aide</h4>
+                    <ul>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Support</a></li>
+                    </ul>
+                </div>
+                <div class="footer-column">
+                    <h4>L'ecosystème</h4>
+                    <ul>
+                        <li><a href="#">Nos partenaires</a></li>
+                        <li><a href="#">Projets</a></li>
+                    </ul>
+                </div>
+                <div class="footer-column">
+                    <h4>A propos</h4>
+                    <ul>
+                        <li><a href="#">Notre histoire</a></li>
+                        <li><a href="#">Equipe</a></li>
+                    </ul>
+                </div>
+                <div class="footer-column">
+                    <h4>Mentions légales</h4>
+                    <ul>
+                        <li><a href="#">Politiques de confidentialité</a></li>
+                        <li><a href="#">Conditions d'utilisation</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <div class="social-icons">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-linkedin"></i></a>
+                </div>
+                <div class="paymeynt-icons">
+                    <img src="logo/visa.png" alt="Visa">
+                    <img src="logo/mastercard.png" alt="Mastercard">
+                    <img src="logo/paypal.png" alt="PayPal">
+                </div>
+            </div>
+        </div>
+ </footer>
+</body>
+</html>
